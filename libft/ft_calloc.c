eaab912f1a82926c653aebdfe448cd604e00f682
Rogerio <Rogerioprogramador2021@gmail.com>
2021-05-19 03:55:52 -0300

    update

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rsiqueir <rsiqueir@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 13:13:11 by rsiqueir          #+#    #+#             */
-/*   Updated: 2021/05/18 13:13:12 by rsiqueir         ###   ########.fr       */
+/*   Updated: 2021/05/19 03:50:31 by rsiqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	char	*calloc1;
 	size_t	a;
 
+	a = 0;
 	calloc1 = malloc(count * size);
 	while (calloc1[a])
 	{
