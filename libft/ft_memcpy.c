8f589d2b3edf105f55b7c3f48dddb86e86723c54
Rogerio <rogerioprogramador2021@gmail.com>
2021-05-29 00:21:35 -0300

    Update

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	a;
 
 	a = 0;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (a < n)
 	{
 		((unsigned char *)dst)[a] = ((unsigned char *)src)[a];
