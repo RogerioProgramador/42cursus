f536132b5195181bf6f05cd453ca1738da0738b2
Rogerio <rogerioprogramador2021@gmail.com>
2021-05-28 21:31:53 -0300

    update

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rogeriorslf <rogeriorslf@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 16:33:02 by rogeriorslf       #+#    #+#             */
-/*   Updated: 2021/05/28 14:45:11 by rogeriorslf      ###   ########.fr       */
+/*   Updated: 2021/05/28 21:29:51 by rogeriorslf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
-	size_t	l;
 
-	i = ft_strlen(dst);
-	j = ft_strlen(src);
-	k = 0;
-	l = i > dstsize;
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	if (dstsize < i)
-		j += dstsize;
-	else
-		j += i;
-	while (src[k] && i < (dstsize - 1))
-	{
-		dst[i] = src[k];
+	i = 0;
+	j = 0;
+	while (dst[i] && i < dstsize)
 		i++;
-		k++;
-	}
-	if (l == 0)
+	while (src[j] != '\0' && i + 1 < dstsize)
+		dst[i++] = src[j++];
+	if (dstsize > i)
 		dst[i] = '\0';
-	return (j);
+	while (src[j++])
+		i++;
+	return (i);
 }
