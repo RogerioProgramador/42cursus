010d72db90727dd54e03ed83bc82b301db60ae6d
Rogerio <rogerioprogramador2021@gmail.com>
2021-05-29 01:07:22 -0300

    Update

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -27,7 +27,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 			return (NULL);
 		return (pointer);
 	}
-	pointer = (char *)malloc((sizeof(*s) * len + 1));
+	pointer = (char *)malloc((sizeof(char) * (len + 1)));
 	if (!pointer)
 		return (NULL);
 	while (count < len)
