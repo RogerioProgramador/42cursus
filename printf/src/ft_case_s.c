f37fab3a2a021846d1e610bc9e7228adeb04f98b
Rogerio <rogerioprogramador2021@gmail.com>
2021-07-12 14:02:02 -0300

    update

--- printf/src/ft_case_s.c
+++ printf/src/ft_case_s.c
@@ -8,7 +8,7 @@ char    *ft_case_s(va_list args)
 
     s_string = va_arg(args, char *);
     if (s_string == NULL)
-        return (NULL);
+        return (ft_strdup("(null)"));
     i = ft_strlen(s_string);
     pointer = (char *)malloc(sizeof(char) * (i + 1));
     pointer[i] = 0;
