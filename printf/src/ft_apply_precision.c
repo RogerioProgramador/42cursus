9faf7bc8aa1e23c925e1e2c5169a7651700ac4c1
Rogerio <rogerioprogramador2021@gmail.com>
2021-07-13 14:29:39 -0300

    update

--- printf/src/ft_apply_precision.c
+++ printf/src/ft_apply_precision.c
@@ -5,7 +5,8 @@ static char    *precision_for_strings(char *pointer, int precision)
     char    *result;
     int     i;
 
-    if (precision >= ft_strlen(pointer) || precision <= 0)
+    i = ft_strlen(pointer);
+    if (precision >= i || precision <= 0)
         return (pointer);
     result = (char *)malloc((precision + 1) * sizeof(char));
     result[precision] = 0;
