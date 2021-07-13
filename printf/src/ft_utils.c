1c4b05c4be410c984f764a082087c01e1cb8ba80
Rogerio <rogerioprogramador2021@gmail.com>
2021-07-13 15:01:45 -0300

    update

--- printf/src/ft_utils.c
+++ printf/src/ft_utils.c
@@ -48,7 +48,7 @@ char    *ft_negfirst(char *s,printparameters *params)
     int     j;
 
     i = ft_strlen(s);
-    if ((!params->precision_bool))
+    if ((!params->precision_bool) || ((params->precision) < i))
         return (s);
     pointer = ft_strchr(s, '-');
     *pointer = '0';
